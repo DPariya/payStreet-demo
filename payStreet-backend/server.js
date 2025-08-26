9dcad72f6e728f732fd3090ae53a75c8954cb2ae
Dipti Pariya <diptipariya@gmail.com>
2025-08-26 22:17:47 +0530

    Add basic packages

--- payStreet-backend/server.js
+++ payStreet-backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
-
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
