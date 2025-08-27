4912b24484de67afffd8d88aee85cb956a7487ac
Dipti Pariya <diptipariya@gmail.com>
2025-08-27 12:04:24 +0530

    Create and add DB

--- payStreet-backend/server.js
+++ payStreet-backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db";
 
 dotenv.config();
-const app = express();
 
+const app = express();
 app.use(express.json());
 app.use(cors());
+
+//Database connection
+connectDB();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
